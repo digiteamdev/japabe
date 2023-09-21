e61e1d387877200a387096643f603605a5e2306b
angga pcaw <anggapcaw@anggas-MacBook-Air.local>
2023-09-21 11:56:43 +0700

    update user

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,68 +1,148 @@
 import { Request, Response } from "express";
 import prisma from "../middleware/user";
+import pagging from "../utils/paggination";
+import url from "url";
 
 const getUser = async (request: any, res: Response) => {
   try {
-    const username = request.session.userId;
-    const user = await prisma.user.findMany({
-      where: { username: username },
-      select: {
-        id: true,
-        username: true,
-        hashed_password: true,
-        employee: {
-          select: {
-            id: true,
-            employee_name: true,
-            NIP: true,
-            NIK: true,
-            NPWP: true,
-            id_card: true,
-            nick_name: true,
-            email: true,
-            birth_place: true,
-            birth_date: true,
-            address: true,
-            province: true,
-            city: true,
-            districts: true,
-            sub_districts: true,
-            ec_postalcode: true,
-            phone_number: true,
-            start_join: true,
-            remaining_days_of: true,
-            gender: true,
-            marital_status: true,
-            sub_depart: true,
-            subdepartId: true,
-            employee_status: true,
-            spouse_name: true,
-            gender_spouse: true,
-            spouse_birth_place: true,
-            spouse_birth_date: true,
-            status_user: true,
-            createdAt: true,
-            updatedAt: true,
-            deleted: true,
+    const pencarian: any = request.query.search || "";
+    const hostname: any = request.headers.host;
+    const pathname = url.parse(request.url).pathname;
+    const page: any = request.query.page;
+    const perPage: any = request.query.perPage;
+    const pagination: any = new pagging(page, perPage, hostname, pathname);
+    const userCount = await prisma.user.count({});
+    let results: any;
+    if (request.query.page === undefined) {
+      const username = request.session.userId;
+      results = await prisma.user.findMany({
+        where: { username: username },
+        select: {
+          id: true,
+          username: true,
+          employee: {
+            select: {
+              id: true,
+              employee_name: true,
+              NIP: true,
+              NIK: true,
+              NPWP: true,
+              id_card: true,
+              nick_name: true,
+              email: true,
+              birth_place: true,
+              birth_date: true,
+              address: true,
+              province: true,
+              city: true,
+              districts: true,
+              sub_districts: true,
+              ec_postalcode: true,
+              phone_number: true,
+              start_join: true,
+              remaining_days_of: true,
+              gender: true,
+              marital_status: true,
+              sub_depart: true,
+              subdepartId: true,
+              employee_status: true,
+              spouse_name: true,
+              gender_spouse: true,
+              spouse_birth_place: true,
+              spouse_birth_date: true,
+              status_user: true,
+              createdAt: true,
+              updatedAt: true,
+              deleted: true,
+            },
           },
-        },
-        userRole: {
-          select: {
-            roleId: true,
-            role: {
-              select: {
-                role_name: true,
+          userRole: {
+            select: {
+              roleId: true,
+              role: {
+                select: {
+                  role_name: true,
+                },
               },
             },
           },
         },
-      },
-    });
-    if (user.length > 0) {
+      });
+    } else {
+      results = await prisma.user.findMany({
+        where: {
+          username: {
+            contains: pencarian,
+          },
+        },
+        select: {
+          id: true,
+          username: true,
+          employee: {
+            select: {
+              id: true,
+              employee_name: true,
+              NIP: true,
+              NIK: true,
+              NPWP: true,
+              id_card: true,
+              nick_name: true,
+              email: true,
+              birth_place: true,
+              birth_date: true,
+              address: true,
+              province: true,
+              city: true,
+              districts: true,
+              sub_districts: true,
+              ec_postalcode: true,
+              phone_number: true,
+              start_join: true,
+              remaining_days_of: true,
+              gender: true,
+              marital_status: true,
+              sub_depart: true,
+              subdepartId: true,
+              employee_status: true,
+              spouse_name: true,
+              gender_spouse: true,
+              spouse_birth_place: true,
+              spouse_birth_date: true,
+              status_user: true,
+              createdAt: true,
+              updatedAt: true,
+              deleted: true,
+            },
+          },
+          userRole: {
+            select: {
+              roleId: true,
+              role: {
+                select: {
+                  role_name: true,
+                },
+              },
+            },
+          },
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+        take: parseInt(pagination.perPage),
+        skip: parseInt(pagination.page) * parseInt(pagination.perPage),
+      });
+    }
+    if (results.length > 0) {
       return res.status(200).json({
         success: true,
         massage: "Get All user",
-        result: user,
+        result: results,
+        page: pagination.page,
+        limit: pagination.perPage,
+        totalData: userCount,
+        currentPage: pagination.currentPage,
+        nextPage: pagination.next(),
+        previouspage: pagination.prev(),
       });
     } else {
       return res.status(404).json({
