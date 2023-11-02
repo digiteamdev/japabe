12b8942889908b52f58a7e3fde10fb56a69de4c6
angga pcaw <anggapcaw@anggas-MacBook-Air.local>
2023-11-02 15:53:46 +0700

    add rate lmit

---
+++ src/utils/rate-limit.ts
@@ -0,0 +1,10 @@
+import setRateLimit from "express-rate-limit";
+
+const rateLimit = setRateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false,
+});
+
+export default rateLimit;
