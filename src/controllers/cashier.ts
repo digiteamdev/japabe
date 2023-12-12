e89a9a230061292d2737b53e14fae39c14507186
angga pcaw <anggapcaw@anggas-MacBook-Air.local>
2023-12-12 15:21:10 +0700

    update add get cashier

--- src/controllers/cashier.ts
+++ src/controllers/cashier.ts
@@ -16,7 +16,8 @@ const getCashier = async (request: Request, response: Response) => {
         deleted: null,
       },
     });
-    let results;
+    let results: any;
+    let cdv: any;
     if (request.query.page === undefined) {
       results = await prisma.kontrabon.findMany({
         where: {
@@ -431,6 +432,41 @@ const getCashier = async (request: Request, response: Response) => {
           createdAt: "desc",
         },
       });
+      cdv = await prisma.cash_advance.findMany({
+        where: {
+          status_manager_director: "approve",
+        },
+        include: {
+          employee: true,
+          user: {
+            select: {
+              id: true,
+              username: true,
+              employee: {
+                select: {
+                  id: true,
+                  employee_name: true,
+                  position: true,
+                },
+              },
+            },
+          },
+          wor: {
+            include: {
+              customerPo: {
+                include: {
+                  quotations: {
+                    include: {
+                      Customer: true,
+                      CustomerContact: true,
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      });
     } else {
       results = await prisma.cashier.findMany({
         where: {
@@ -840,11 +876,16 @@ const getCashier = async (request: Request, response: Response) => {
         skip: parseInt(pagination.page) * parseInt(pagination.perPage),
       });
     }
-    if (results.length > 0) {
+    let obj: any = [];
+    const cashAdvance: any = [cdv];
+    cashAdvance.map((d: any) => {
+      obj.push(...d);
+    });
+    if (results.length > 0 || obj.length > 0) {
       return response.status(200).json({
         success: true,
         massage: "Get All Cashier",
-        result: results,
+        result: [...results, ...obj],
         page: pagination.page,
         limit: pagination.perPage,
         totalData: cashieCount,
